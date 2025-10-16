aa32266b606e021effc1239eb09d9f32ca90fbee
Himanshu <himanshubisht0926@gmail.com>
2025-10-16 16:34:29 +0530

    add

---
+++ index1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    // area of circle
+    int r=7;
+    int area;
+    printf("Enter radius of circle:");
+    scanf("%d",&r);
+    area=3.14*r*r;
+    printf("Area of circle is %d",area);
+    return 0;
+
+}
